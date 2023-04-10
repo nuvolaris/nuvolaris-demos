31eb524c2675ebbf7bbc3e7867023a6c41246d4c
Michele Sciabarra <michele@nuvolaris.io>
2023-04-10 20:14:56 +0100

    reformat

--- sendgrid-js/nodefetch.js
+++ sendgrid-js/nodefetch.js
@@ -7,9 +7,11 @@ async function main(args) {
             'Authorization': 'Bearer ' + args.sendgrid
         },
         body: JSON.stringify({
-            "personalizations": [{ "to": [{ "email": args.to }] }],
             "from": { "email": args.from },
             "subject": "Sending with SendGrid is Fun",
+            "personalizations": [
+                { "to": [{ "email": args.to }] }
+            ],
             "content": [
                 { "type": "text/plain", "value": "and easy to do anywhere, even with cURL" }
             ]
